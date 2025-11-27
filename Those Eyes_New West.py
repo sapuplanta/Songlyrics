e19067f4a6b7a6b8317dfe1fd0b0fb3e6929f6e1
sapuplanta <jevaanraj17@gmail.com>
2025-11-27 22:06:52 +0800

    Add lyrics to print_lyrics function

--- Those Eyes_New West.py
+++ Those Eyes_New West.py
@@ -7,4 +7,15 @@ def type_lyric(line, char_delay=0.065):
     print()
 
 def print_lyrics():
+    lyrics = [
+        "Cause all of the small",
+        "things that you do",
+        "Are what remind me",
+        "why I fell for you",
+        "And when we're apart ",
+        "and I'm missing you",
+        "I close my eyes and all I see is you",
+        "And the small things you do"
+    ]
+
     
