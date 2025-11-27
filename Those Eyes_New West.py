966bee17a9c321076953cd443fc1c9d546fde1a1
sapuplanta <jevaanraj17@gmail.com>
2025-11-27 22:03:38 +0800

    Add initial implementation of lyric typing and printing functions

---
+++ Those Eyes_New West.py
@@ -0,0 +1,10 @@
+import time
+
+def type_lyric(line, char_delay=0.065):
+    for char in line:
+        print(char, end='', flush=True)
+        time.sleep(char_delay)
+    print()
+
+def print_lyrics():
+    
